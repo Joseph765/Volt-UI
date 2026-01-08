ac904f9bdfaba6b628895be50d30bd4e517d5436
Joseph765 <josephchauvin@Josephs-MacBook-Air.local>
2026-01-08 13:43:50 -0600

    added divider documentation

---
+++ src/routes/components/divider/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import {
+        CodeExample,
+        Divider,
+        Flex,
+        Select,
+        Text
+    } from "$lib";
+
+    let options = [
+        { value: 'basic', label: 'Example: Basic' }
+    ];
+
+    let selectedExample = 'basic';
+
+    let example1 = `<Divider />`;
+</script>
+
+<Flex direction="column" gap="xl">
+    <Flex direction="column">
+        <Text size="3xl" weight="black" as="h2">Divider</Text>
+        <Text size="xl" variant="weak">A lightweight horizontal line that visually separates content.</Text>
+    </Flex>
+    <Flex direction="column">
+        <Select
+            name="examples"
+            bind:options={options}
+            bind:value={selectedExample}
+            placeholder="Select an example..."
+        />
+        {#if selectedExample === "basic"}
+            <CodeExample code={example1}>
+                <Divider />
+            </CodeExample>
+        {/if}
+    </Flex>
+</Flex>
